0f5ca211a0ba95eb274458a4dad5ea818dc4eedf
Nedopekin Maxim <nedopekin.maxim@gmail.com>
2022-10-03 13:10:31 +0300

    .

--- app/src/main/java/com/example/deliveryappandroid/screens/MainScreen.kt
+++ app/src/main/java/com/example/deliveryappandroid/screens/MainScreen.kt
@@ -1,10 +1,15 @@
 package com.example.deliveryappandroid.mainscreens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
-
+@Preview
 @Composable
 fun MainScreen(navController: NavController) {
+
+
+
+
 
 }
